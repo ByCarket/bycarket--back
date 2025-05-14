34022313d938a673c945da52f9b9b4f5253caa6c
germanparietti <germanparietti@gmail.com>
2025-05-14 09:35:16 -0300

    feat:Swagger implemented

--- src/main.ts
+++ src/main.ts
@@ -4,27 +4,25 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
   
   const swaggerConfig = new DocumentBuilder().setTitle
   ('Bycarket API')
-    .setDescription('API documentation for Bycarket')
-    .setVersion('1.0')
-    .addTag('bycarket')
-    .build();
-    
-    const document = SwaggerModule.createDocument(app, swaggerConfig);
-    SwaggerModule.setup('api', app, document,{
-      swaggerOptions:{
-        tagsSorter:(a: string, b: string) => {
-          const order = ['Users', 'Vehicles',
-            'Brands', 'Models', 'YearOptions', 'Images', 'Orders', 'Payments', 'Auth'
-          ];
-          return order.indexOf(a) - order.indexOf(b);
-        },
-        operationsSorter: 'method',
-      }}
-    );
+  .setDescription('API documentation for Bycarket')
+  .setVersion('1.0')
+  .addTag('bycarket')
+  .build();
+  
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document,{
+    swaggerOptions:{
+      tagsSorter:(a: string, b: string) => {
+        const order = ['Vehicles'];
+        return order.indexOf(a) - order.indexOf(b);
+      },
+      operationsSorter: 'method',
+    }}
+  );
+  await app.listen(process.env.PORT ?? 3000);
   }
 
 
