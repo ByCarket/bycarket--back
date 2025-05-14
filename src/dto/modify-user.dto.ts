423c4f4b7d3f285494d3a32b2c006bd958468b53
agustinchoque <agustinchoque.dev@gmail.com>
2025-05-14 20:12:56 -0300

    fix(dto): DTO for new update method

--- src/dto/modify-user.dto.ts
+++ src/dto/modify-user.dto.ts
@@ -1,6 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { OmitType } from '@nestjs/swagger';
 
-export class ModifyUserDto extends PartialType(CreateUserDto) {
-  orders?: { id: string; date: Date }[];
-}
+export class ModifyUserDto extends OmitType(PartialType(CreateUserDto), [
+  'email',
+  'password',
+  'confirmPassword',
+] as const) {}
