3d6caef1dfd27d21df7f5ae7d9749e7d4b34abc4
Frysccou <franespindola71@gmail.com>
2025-05-15 12:24:58 -0300

    feat: enable CORS and add related dependencies
    Enable CORS in the application to allow cross-origin requests and add necessary dependencies (`cors` and `@types/cors`) to support this functionality.

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Bycarket API')
     .setDescription('API documentation for Bycarket')
