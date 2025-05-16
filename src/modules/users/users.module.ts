d9d542d4b96d097c8f9f16bf31e9d7e2f552c466
agustinchoque <agustinchoque.dev@gmail.com>
2025-05-16 01:10:07 -0300

    feat: add vehicle relationship to user entity and update related modules

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -3,11 +3,10 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
+import { Post } from 'src/entities/post.entity';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([User]),
-  ],
+  imports: [TypeOrmModule.forFeature([User, Post])],
   providers: [UsersService],
   controllers: [UsersController],
   exports: [UsersService],
