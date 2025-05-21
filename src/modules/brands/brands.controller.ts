fadf55e1532211082586856eb6a1ea96c27cd318
agustinchoque <agustinchoque.dev@gmail.com>
2025-05-21 11:17:59 -0300

    Remove Public decorators from findAll and findOne methods in BrandsController to enforce role-based access control

--- src/modules/brands/brands.controller.ts
+++ src/modules/brands/brands.controller.ts
@@ -18,38 +18,37 @@ import { AuthGuard } from 'src/guards/auth.guard';
 import { Role } from 'src/enums/roles.enum';
 import { Roles } from 'src/decorators/roles.decorator';
 import { RolesGuard } from 'src/guards/roles.guard';
-import { Public } from 'src/decorators/publicRoutes.decorator';
 
 @ApiTags('Brands')
 @Controller('brands')
 @ApiBearerAuth()
 @UseGuards(AuthGuard, RolesGuard)
-@Roles(Role.ADMIN)
 export class BrandsController {
   constructor(private readonly service: BrandsService) {}
 
-  @Public()
   @Get()
   findAll(): Promise<Brand[]> {
     return this.service.findAll();
   }
 
-  @Public()
   @Get(':id')
   findOne(@Param('id', ParseUUIDPipe) id: string): Promise<Brand> {
     return this.service.findOne(id);
   }
 
+  @Roles(Role.ADMIN)
   @Post()
   create(@Body() dto: CreateBrandDto): Promise<Brand> {
     return this.service.create(dto);
   }
 
+  @Roles(Role.ADMIN)
   @Put(':id')
   update(@Param('id', ParseUUIDPipe) id: string, @Body() dto: UpdateBrandDto): Promise<Brand> {
     return this.service.update(id, dto);
   }
 
+  @Roles(Role.ADMIN)
   @Delete(':id')
   delete(@Param('id', ParseUUIDPipe) id: string): Promise<void> {
     return this.service.delete(id);
