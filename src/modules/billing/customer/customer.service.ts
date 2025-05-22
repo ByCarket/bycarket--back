3cd41dfc8f44a4453ca024124ab7f925942509a6
agustinchoque <agustinchoque.dev@gmail.com>
2025-05-22 12:33:27 -0300

    feat: implement customer DTOs and update CustomerService methods for Stripe integration

--- src/modules/billing/customer/customer.service.ts
+++ src/modules/billing/customer/customer.service.ts
@@ -1,17 +1,24 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { CreateCustomerDto } from 'src/DTOs/billingDto/customerDto/createCustomer.dto';
+import { UpdateCustomerDto } from 'src/DTOs/billingDto/customerDto/updateCustomer.dto';
 import { STRIPE_CLIENT } from 'src/providers/stripe.provider';
 import Stripe from 'stripe';
 
 @Injectable()
 export class CustomerService {
   constructor(@Inject(STRIPE_CLIENT) private readonly stripe: Stripe) {}
-
-  async createCustomer(createCustomerDto): Promise<Stripe.Customer> {
-    return await this.stripe.customers.create(createCustomerDto);
+  async getCustomer(id: string) {
+    return await this.stripe.customers.retrieve(id);
   }
 
-  async updateCustomer(id: string, updateCustomerDto): Promise<Stripe.Customer> {
-    return await this.stripe.customers.update(id, updateCustomerDto);
+  async createCustomer(createCustomerDto: CreateCustomerDto): Promise<string> {
+    const customer = await this.stripe.customers.create(createCustomerDto);
+    return customer.id;
+  }
+
+  async updateCustomer(id: string, updateCustomerDto: UpdateCustomerDto): Promise<string> {
+    await this.stripe.customers.update(id, updateCustomerDto);
+    return id;
   }
 
   async deleteCustomer(id: string): Promise<Stripe.DeletedCustomer> {
