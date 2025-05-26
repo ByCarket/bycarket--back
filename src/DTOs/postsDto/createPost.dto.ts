0fa4349d2aede37fe8431f352f341d47c6eef102
heyfel <heyfel232@gmail.com>
2025-05-26 11:49:28 -0300

    feat: update CreatePostDto to make description optional and modify vehicle description update logic

--- src/DTOs/postsDto/createPost.dto.ts
+++ src/DTOs/postsDto/createPost.dto.ts
@@ -1,12 +1,13 @@
 import { ApiProperty, PickType } from '@nestjs/swagger';
 import { BasePostDto } from './basePosts.dto';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreatePostDto extends PickType(BasePostDto, ['vehicleId'] as const) {
   @ApiProperty({
     description: 'Posible descripcion nueva del vehiculo',
+    required: false
   })
-  @IsNotEmpty()
+  @IsOptional()
   @IsString()
-  description: string;
+  description?: string;
 }
