309bbe85e4e8156aa6ea6e1563ce830a4cf69b41
Juanjo Ramirez <130495380+Juanjo-Ramirez@users.noreply.github.com>
2025-06-03 22:27:50 -0300

    Revert "fix(webhooks): update subscription handling to include raw body and s…"

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,7 @@ import { LoggerGlobal } from './middlewares/logger.middleware';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    rawBody: true,
-  });
+  const app = await NestFactory.create(AppModule);
   app.enableCors();
 
   const swaggerConfig = new DocumentBuilder()
